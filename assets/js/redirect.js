2f7064dbc3644d0fa2c07cd78792a8b8d2097079
keerti.nandihalli <keertinandi-reisys>
2016-08-10 12:26:45 -0400

    alert test

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -2,6 +2,8 @@
  * Created by keerti.nandihalli on 8/10/16.
  */
 
-$("a").click(function(){
-  alert("u left");
+$(document).ready(function(){
+    $("a").click(function(){
+        alert("hello world!");
+    });
 });
