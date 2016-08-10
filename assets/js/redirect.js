d4765c985b87509e8eab255bfbabf1cae0970e52
keerti.nandihalli <keertinandi-reisys>
2016-08-10 12:15:19 -0400

    alert test

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,4 +1,15 @@
 /**
  * Created by keerti.nandihalli on 8/10/16.
  */
-alert("hello world");
+var comp = new RegExp(location.host);
+
+$('a').each(function(){
+    if(comp.test($(this).attr('href'))){
+        // a link that contains the current host
+        $(this).addClass('local');
+    }
+    else{
+        // a link that does not contain the current host
+        $(this).addClass('external');
+    }
+});
