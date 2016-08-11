5cb90f5c120983764a6c1b278bc289a71a5ef3f3
keerti.nandihalli <keertinandi-reisys>
2016-08-11 11:55:34 -0400

    error message

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -2,8 +2,14 @@
  * Created by keerti.nandihalli on 8/10/16.
  */
 
-$(document).ready(function(){
+/*$(document).ready(function(){
     $('a[href*="htt"]').click(function() {
+        alert("You are now leaving Datacenters.cio.gov");
+    });
+});*/
+
+$(document).ready(function(){
+    $('a[target="_blank"]').click(function() {
         alert("You are now leaving Datacenters.cio.gov");
     });
 });
